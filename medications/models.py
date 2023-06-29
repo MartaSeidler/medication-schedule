14210d06400913248aa1d1e8edd63ad4ba0db648
MartaSeidler <martaseidler2312@gmail.com>
2023-06-29 21:17:31 +0200

    Update models

--- medications/models.py
+++ medications/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 UNITS = {
@@ -11,13 +12,13 @@ UNITS = {
 
 class Medication(models.Model):
     name_of_medication = models.CharField(default="", max_length=64, blank=False, unique=False)
-    time_1 = models.TimeField(null=True, blank=True)
+    time_1 = models.TimeField(auto_now=False, default=timezone.now)
     dosage_1 = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
     unit_1 = models.CharField(choices=UNITS, max_length=10, default='ml')
-    time_2 = models.TimeField(null=True, blank=True)
+    time_2 = models.TimeField(auto_now=False, default=timezone.now)
     dosage_2 = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
     unit_2 = models.CharField(choices=UNITS, max_length=10, default='ml')
-    time_3 = models.TimeField(null=True, blank=True)
+    time_3 = models.TimeField(auto_now=False, default=timezone.now)
     dosage_3 = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
     unit_3 = models.CharField(choices=UNITS, max_length=10, default='ml')
     picture = models.ImageField(upload_to="pictures", blank=True, null=True)
